d2e8fd7ea2396f27d70ccf120c3a44cc3f0dcd1e
VAISHNAVI GANESH <vaishuganesh0709@gmail.com>
2026-06-10 12:17:47 +0530

    Add NumberPattern class to print number pattern

---
+++ Level1_Task2_NumberPattern/NumberPattern.java
@@ -0,0 +1,14 @@
+// NumberPattern.java
+public class NumberPattern {
+    public static void main(String[] args) {
+
+        int rows = 5;
+
+        for (int row = 1; row <= rows; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+    }
+}
